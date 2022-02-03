2b94d726aa7d5ffea7e8d7f1acad3ce00e499a7d
Michael Hensel <mhensel@ycp.edu>
2022-02-03 16:31:50 -0500

    changed size from 40 to 50

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
@@ -84,8 +84,8 @@ public class GameView extends JPanel {
 				Square square = new Square();
 				square.setX(300.0);
 				square.setY(220.0);
-				square.setWidth(40.0);
-				square.setHeight(40.0);
+				square.setWidth(50.0);
+				square.setHeight(50.0);
 				model.setSquare(square);
 				
 				GameController controller = new GameController();
